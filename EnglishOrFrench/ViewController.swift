3be264eaad4a372f51b735fd3a395e0807a4e3ac
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2019-11-26 10:44:31 -0500

    Initial Commit

---
+++ EnglishOrFrench/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EnglishOrFrench
+//
+//  Created by Fulcherberguer, Fernanda on 2019-11-26.
+//  Copyright © 2019 Fulcherberguer, Fernanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

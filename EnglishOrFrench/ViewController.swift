24077d8c8e7ca4d4920216f3ad3d34db9ba7d85e
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2019-11-28 14:24:30 -0500

    Finished the app

--- EnglishOrFrench/ViewController.swift
+++ EnglishOrFrench/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    //MARK: Properties
+    
+    
+    
+    
+    
+    //Create Outlets
+    @IBOutlet weak var phraseInputField: UITextField!
+    
+    @IBOutlet weak var outputResult: UILabel!
+    
+    //MARK: Methods
+    //Functions (make things happen)
+    //Actions go here
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func buttonAnalyze(_ sender: Any) {
+        
+        //1. INPUT
+        //Clear out the output label from the last time the Analyze button was pressed
+        outputResult.text = ""
+        
+        //Guard against no input
+        guard let phraseInput = phraseInputField.text, phraseInput.count > 0 else {
+            outputResult.text = "Please enter a phrase to analyze."
+            return
+            
+        }
+        
+        // Created happy emojis and sad emojis constantss
+        let englishArray = "😃😊😄🙂"
+        let frenchArray = "☹🙁😕😔"
+        
+        
+    }
+    
 }
-
